49d0dfaaf9b3d5d043acde44dc56588ea5150af3
foreverxzhh <foreverxzhh@gmail.com>
2019-06-14 13:51:43 +0800

    Initial commit

---
+++ app/src/main/java/com/hua/draganddraw/DragAndDrawFragment.java
@@ -0,0 +1,28 @@
+package com.hua.draganddraw;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DragAndDrawFragment extends Fragment {
+
+    public static DragAndDrawFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        DragAndDrawFragment fragment = new DragAndDrawFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_drag_and_draw, container, false);
+        return v;
+    }
+}
